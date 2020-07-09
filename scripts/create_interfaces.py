5a46a1e9f274bfbf79c99599a5510e15dcde3f4b
Ildar Garaev <ildafon@yandex.ru>
2020-07-09 15:30:02 +0300

    create_interface

--- scripts/create_interfaces.py
+++ scripts/create_interfaces.py
@@ -13,7 +13,7 @@ nr = InitNornir(config_file="./config.yaml")
 nb_url, nb_token, ssl_verify = nr.config.inventory.options.values()
 nb_host = re.sub("^.*//|:.*$", "", nb_url)
 
-netbox = NetBox(host=nb_host, port=32768, use_ssl=False, auth_token=nb_token)
+netbox = NetBox(host=nb_host, port=32768, use_ssl=True, auth_token=nb_token)
 nb_interfaces = netbox.dcim.get_interfaces()
 
 
